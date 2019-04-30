f808ccff2808ed75b9a02980cfdabdba38811d2f
Rebecca Wedow <rwedow@ufl.edu>
2019-04-30 13:10:06 -0400

    points and ammo bars complete and functional

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -8,15 +8,18 @@ public class Rain : MonoBehaviour
 
     private float startHealth = 100;
     private float health;
-    public int points;
+    private float startPoints = 0;
+    private float points;
     public Animator anim;
 
     public Image healthBar;
+    public Image pointsBar;
 
     // Start is called before the first frame update
     void Start()
     {
         health = startHealth;
+        points = startPoints;
     }
 
     // Update is called once per frame
@@ -26,7 +29,15 @@ public class Rain : MonoBehaviour
     public void TakeDamage(int damage) {
         if (!anim.GetBool("Umbrella")) {
             health -= damage;
-            healthBar.fillAmount = health / startHealth;;
+            healthBar.fillAmount = health / startHealth;
         }
     }
+
+    public void GetPoints(int newPoints) {
+        points += newPoints;
+        pointsBar.fillAmount = points;
+    }
+
+    //add get paint bucket here
+    //paintBar.fillAmount = points;
 }
