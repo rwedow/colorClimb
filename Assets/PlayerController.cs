c0b6063e73e6b37e83e6aee9b65c2d7f9fced042
Stephen Wehlburg <swehlburg@ufl.edu>
2019-04-30 11:04:52 -0400

    AAAHHHH

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce;
+    public float speed;
+    public float maxSpeed;
+    public SpriteRenderer sr;
+    public double drag;
+
+    private Rigidbody2D rb;
+    private Vector2 jump = new Vector2(0.0f, 1.0f);
+    bool ground;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        sr.color = Color.white;
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb.AddForce(movement * speed);
+
+        if (movement.magnitude < 0.1)
+        {
+            rb.velocity = new Vector2((float)(rb.velocity.x / drag), rb.velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+            if (rb.velocity.x > maxSpeed)
+            {
+                rb.velocity.Set(maxSpeed, rb.velocity.y);
+            }
+            else if (rb.velocity.x < -maxSpeed)
+            {
+                rb.velocity.Set(-maxSpeed, rb.velocity.y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformPainter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlatformPainter : MonoBehaviour
+{
+    Tilemap platforms;
+    Tilemap paint;
+    public Sprite wetPaint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platforms = GetComponent<Tilemap>();
+        paint = transform.parent.gameObject.transform.parent.gameObject.transform.GetChild(0).transform.Find("Paint").GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        //Debug.Log("Collided with Tilemap");
+        if (col.gameObject.tag == "Painter")
+        {
+            Vector3 painterPos = col.gameObject.GetComponent<Transform>().position;
+            //Debug.Log(playerPos);
+            Vector3Int pos = platforms.WorldToCell(painterPos);
+            pos.y -= 2;
+            TileBase tileAtPos = platforms.GetTile(pos);
+            if (tileAtPos.ToString().Substring(0, 6) == "floor ")
+            {
+                //monochrome tile, switch to colored
+                Debug.Log("Colored tile at " + painterPos);
+                Tile newPaint = new Tile();
+                newPaint.sprite = wetPaint;
+                //newPaint.color = col.getComponent<Painter>.color;
+                newPaint.color = Color.blue;
+                paint.SetTile(pos, newPaint);
+            }
+        }
+
+        else if (col.gameObject.tag == "Enemy")
+        {
+            /*
+            //tell the enemy what's in front of it
+            Vector3 enemyPos = col.gameObject.GetComponent<Transform>().position;
+            int left = tileAtPosition(enemyPos, false);
+            int right = tileAtPosition(enemyPos, true);
+            Vector2Int result = new Vector2Int(left, right);
+            //Debug.Log(result);
+            if (result != null)
+            {
+                Debug.Log(result);
+                col.gameObject.SendMessage("CanMove", result, SendMessageOptions.DontRequireReceiver);
+            }
+            */
+        }
+    }
+}
